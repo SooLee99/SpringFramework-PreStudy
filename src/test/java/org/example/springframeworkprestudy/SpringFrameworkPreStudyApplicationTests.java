d053346960642531f2f709d5a33c2e674de70f3b
Soo Lee <dltn990930@naver.com>
2024-07-30 22:13:14 +0900

    feat: #1 - 스프링 프레임워크 프로젝트 시작 (2024-07-30 이수)
    스프링 프로젝트 초기화
    - spring web
    - lombok
    - spring security
    - spring data jpa
    - mysql driver

---
+++ src/test/java/org/example/springframeworkprestudy/SpringFrameworkPreStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springframeworkprestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkPreStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
